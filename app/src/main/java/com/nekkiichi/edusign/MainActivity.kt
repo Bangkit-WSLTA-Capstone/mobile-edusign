d3b5e9c4ef6793d8e5ff201dd8a9599295259a5f
Agung Adhinata <adhi0asta@gmail.com>
2024-05-21 15:16:40 +0800

    feat: setup custom theme, google fonts, and icon extended

--- app/src/main/java/com/nekkiichi/edusign/MainActivity.kt
+++ app/src/main/java/com/nekkiichi/edusign/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nekkiichi.edusign.ui.composable.PrimaryButton
+import com.nekkiichi.edusign.ui.theme.AppTypography
 import com.nekkiichi.edusign.ui.theme.EduSignTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +24,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            EduSignTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            EduSignTheme(dynamicColor = false) {
+                Scaffold(modifier = Modifier
+                    .fillMaxSize()
+                    .safeDrawingPadding()) { innerPadding ->
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        name = "Android", modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -32,16 +39,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(8.dp)) {
+        PrimaryButton(onCLick = { /*TODO*/ }) {
+            Text(text = "Hello bro", style = AppTypography.labelLarge)
+        }
+        Text(
+            text = "Hello $name!", modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    EduSignTheme {
-        Greeting("Android")
+    EduSignTheme(dynamicColor = false) {
+        Greeting(name = "Android")
     }
 }
---
+++ app/src/main/java/com/nekkiichi/edusign/ui/composable/PrimaryButton.kt
@@ -0,0 +1,53 @@
+package com.nekkiichi.edusign.ui.composable
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nekkiichi.edusign.ui.theme.AppTypography
+import com.nekkiichi.edusign.ui.theme.EduSignTheme
+
+@Composable
+fun PrimaryButton(
+    onCLick: () -> Unit, modifier: Modifier = Modifier, content: @Composable() (RowScope.() -> Unit)
+) {
+    Button(onClick = onCLick, modifier = modifier, shape = RoundedCornerShape(4.dp)) {
+        ProvideTextStyle(value = AppTypography.titleSmall) {
+            content()
+        }
+    }
+}
+
+
+@Preview
+@Composable
+private fun ButtonPreview() {
+    EduSignTheme(dynamicColor = false) {
+        Surface() {
+            Row(
+                modifier = Modifier.padding(4.dp),
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                PrimaryButton(onCLick = { /*TODO*/ }) {
+                    Text(text = "Primary Button")
+                }
+                PrimaryButton(onCLick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.Settings, contentDescription = "Settings icon")
+                    Text(text = "Primary Button with icon")
+                }
+            }
+        }
+    }
+}
