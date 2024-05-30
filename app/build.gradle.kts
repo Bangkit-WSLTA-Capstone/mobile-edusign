98c01eed3642dfcaae85e25152bc3d3d889379e7
Agung Adhinata <adhi0asta@gmail.com>
2024-05-30 11:07:42 +0800

    feat: add markdown renderer android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ android {
 
 dependencies {
 
+    implementation(libs.multiplatform.markdown.renderer.android)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
