4f7c0e41511bee33c607187f2dae8c51a880dfc2
Agung Adhinata <adhi0asta@gmail.com>
2024-06-11 18:17:21 +0800

    data: prepare repository

---
+++ app/src/main/java/com/nekkiichi/edusign/data/EdusignRepository.kt
@@ -0,0 +1,28 @@
+package com.nekkiichi.edusign.data
+
+import java.io.File
+
+class EdusignRepository {
+
+    fun login(email: String, password: String) {
+
+    }
+
+    fun register(username: String, email: String, password: String) {
+
+    }
+
+    // remove token from android app
+    fun logout() {
+
+    }
+
+
+    fun translateVideo(videoFile: File) {
+
+    }
+
+    fun getTranslateHistories() {
+
+    }
+}
